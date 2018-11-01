8b833c24bb091d2c043bf60396af82a5b0440ba7
Ujwal Kumar <Xankill3r@users.noreply.github.com>
2018-11-01 12:23:32 +0530

    Create dice-set-roller.js

---
+++ dice-set-roller.js
@@ -0,0 +1,30 @@
+var setCount = 3;
+var setSize = 6;
+var diceSides = 6;
+var diceRolls = 4;
+
+function init() {
+    for (var i = 0; i < setCount; i++) {
+        generateAndDisplayStatSet();
+    }
+}
+
+function generateAndDisplayStatSet() {
+    var message = "";
+    for (var i = 0; i < setSize; i++) {
+        var curTotal = 0, curMin = diceSides + 1;
+        for (var j = 0; j < diceRolls; j++) {
+            var val = getRandomInt(diceSides) + 1;
+            curTotal += val;
+            curMin = val < curMin ? val : curMin;
+            message += val + (j == diceRolls - 1 ? "" : ", ");
+        }
+        message += " : " + (curTotal - curMin);
+        var textNode = document.createTextNode(message);
+        document.body.appendChild(textNode);
+    }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
