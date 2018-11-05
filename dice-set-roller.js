88080652db63f953a913a56e9c9672942074f9e4
Ujwal Kumar <ujwal.kumar@immersiveartsstudio.com>
2018-11-05 14:01:35 +0530

    Output is displayed inside a separate div Added a button for copying the output to the clipboard

--- dice-set-roller.js
+++ dice-set-roller.js
@@ -5,13 +5,13 @@ var diceRolls = 4;
 
 function init() {
     for (var i = 0; i < setCount; i++) {
-        document.body.appendChild(document.createTextNode("Set " + (i + 1)));
+        document.getElementById("stats").appendChild(document.createTextNode("Set " + (i + 1)));
         generateAndDisplayStatSet();
     }
 }
 
 function generateAndDisplayStatSet() {
-	document.body.appendChild(document.createElement("br"));
+	document.getElementById("stats").appendChild(document.createElement("br"));
     for (var i = 0; i < setSize; i++) {
         var curTotal = 0, curMin = diceSides + 1;
         var message = "";
@@ -23,12 +23,48 @@ function generateAndDisplayStatSet() {
         }
         message += " : " + (curTotal - curMin);
         var textNode = document.createTextNode(message);
-        document.body.appendChild(textNode);
-        document.body.appendChild(document.createElement("br"));
+        document.getElementById("stats").appendChild(textNode);
+        document.getElementById("stats").appendChild(document.createElement("br"));
     }
-	document.body.appendChild(document.createElement("br"));
+	document.getElementById("stats").appendChild(document.createElement("br"));
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function copyStats() {
+    copyTextToClipboard(document.getElementById("stats").innerText);
+}
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function() {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function(err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
 }
